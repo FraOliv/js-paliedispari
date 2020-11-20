db678b7a4e3b0bb77846ed7fc6ec86472e67109e
FRANCESCO <OLIVOTTI>
2020-11-20 14:49:48 +0100

    palindromo check completed

---
+++ palindromo/assets/js/script.js
@@ -0,0 +1,18 @@
+//Palidroma
+//Chiedere all’utente di inserire una parola
+
+//Creare una funzione per capire se la parola inserita è palindroma
+
+var isPalindrome = function (string) {
+    if (string == string.split('').reverse().join('')) {
+        alert(string + ' is palindrome.');
+    }
+    else {
+        alert(string + ' is not palindrome.');
+    }
+}
+
+document.getElementById('form_id').onsubmit = function() {
+   isPalindrome(document.getElementById('your_input').value);
+    return false;
+}
